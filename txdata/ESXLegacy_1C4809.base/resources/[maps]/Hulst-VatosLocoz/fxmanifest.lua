78afe3cbc637a541fd2c89b417770471f3b229c6
hulstrp <167028744+hulstrp@users.noreply.github.com>
2024-04-14 23:59:26 +0200

    mlo3

---
+++ txdata/ESXLegacy_1C4809.base/resources/[maps]/Hulst-VatosLocoz/fxmanifest.lua
@@ -0,0 +1,23 @@
+shared_script '@STILLEAC/shared_fg-obfuscated.lua'
+shared_script '@STILLEAC/ai_module_fg-obfuscated.lua'
+shared_script '@vr-huisinbraak/shared_fg-obfuscated.lua'
+shared_script '@antisteeldienst/shared_fg-obfuscated.lua'
+
+
+
+shared_script "@sac/sentic_init.lua"
+
+fx_version "bodacious"
+games { "gta5" }
+this_is_a_map "yes"
+
+
+Description '-'
+
+Made_by 'Igro45'
+
+Discord 'https://discord.gg/2bjt66Z43r'
+
+Twitch 'https://twitch.tv/igro45'
+
+YouTube 'https://www.youtube.com/channel/UCv-Bl3W4KJOwXnJKBqG8gTA'
