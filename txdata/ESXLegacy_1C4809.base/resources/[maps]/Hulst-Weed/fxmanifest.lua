af99ba23d743ab2937f2f7e35ff671dd8377da06
hulstrp <167028744+hulstrp@users.noreply.github.com>
2024-04-14 23:58:54 +0200

    mlo2

---
+++ txdata/ESXLegacy_1C4809.base/resources/[maps]/Hulst-Weed/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+this_is_a_map 'yes'
+
+Author 'Rfc_Mapping'
+description 'Illegal Business'
+
+version '2.0'
+
+dependency '/assetpacks'
+
+dependency '/assetpacks'
